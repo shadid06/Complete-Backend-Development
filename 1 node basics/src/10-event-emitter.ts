29f66f8be6af41bba74748fa13c4fafef04bc48d
shadid06 <hasnainbauet@gmail.com>
2026-08-09 13:08:02 +0600

    git ignore add

--- 1 node basics/src/10-event-emitter.ts
+++ 1 node basics/src/10-event-emitter.ts
@@ -8,3 +8,15 @@
 
 // notify some other service
 
+// emit one event -> listeners listen to this event and do something
+
+// .on() - register one listener
+// .once() - register one listener (only once) and runs one time
+// .emit() - emit one event
+
+
+// event name - string
+// callback function - handler
+
+
+import { EventEmitter } from "node:events";
