d6a20e9b489958362cd8edbe5b55b236a660af68
shadid06 <hasnainbauet@gmail.com>
2026-08-09 15:28:37 +0600

    till strreams

--- 1 node basics/src/10-event-emitter.ts
+++ 1 node basics/src/10-event-emitter.ts
@@ -20,3 +20,50 @@
 
 
 import { EventEmitter } from "node:events";
+
+const appEvents=new EventEmitter();
+
+type UserRegistratinPayload={
+    userId:string,
+    email:string
+}
+
+type UserRegistrationEvent="user:registered";
+
+//Register listener for user:registered event
+appEvents.on("user:registered",(payload:UserRegistratinPayload)=>{
+    console.log("User registration event received");
+    console.log("payload: ",payload);
+})
+
+//register another listener
+appEvents.on("user:registered",(payload:UserRegistratinPayload)=>{
+    console.log("sending welcome email to ",payload.email);
+})
+
+
+//register listener for user:registered event only once
+appEvents.once("user:registered",(payload:UserRegistratinPayload)=>{
+    console.log("This will run only one time");
+})
+
+
+//emit event after 2 seconds
+setTimeout(()=>{
+    const payload:UserRegistratinPayload={
+        userId:"123",
+        email:"example@example.com"
+    };
+    console.log("Emitting user:registered event");
+    appEvents.emit("user:registered",payload);
+},2000)
+
+//emit event after 4 seconds
+setTimeout(()=>{
+    const payload:UserRegistratinPayload={
+        userId:"124",
+        email:"example2@example.com"
+    };
+    console.log("Emitting user:registered event");
+    appEvents.emit("user:registered",payload);
+},4000)
