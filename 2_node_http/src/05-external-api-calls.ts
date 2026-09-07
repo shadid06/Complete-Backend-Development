e1414761cbbf714308464f811817d50d169948e2
shadid06 <hasnainbauet@gmail.com>
2026-09-07 10:06:34 +0600

    node http done

--- 2_node_http/src/05-external-api-calls.ts
+++ 2_node_http/src/05-external-api-calls.ts
@@ -35,12 +35,46 @@ export interface User {
     id:number;
     name:string;
     email:string;
+    company:string;
 }
 
 function transformUser(user:User):MyInterface{
     return {
         id:user.id,
         name:user.name,
-        email:user.email
+        email:user.email,
+        company:user.company.name
     }
 }
+
+async function fetchExternalUser():Promise<void> {
+  //we can abort a request if it takes more than given time i.e. 1 second
+  const controller=new AbortController();
+  const timeoutId=setTimeout(()=>controller.abort(),1000);
+  try {
+    const response=await fetch(API_URL,
+      { method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+        signal: controller.signal });
+    if (!response.ok) {
+        throw new Error(`Request failed with status code: ${response.status}`);
+        
+    }
+    const user:User=await response.json();
+    console.log(user);
+    console.log(transformUser(user));
+    
+  } catch (error:any) {
+    if(error.name==='AbortError'){
+      console.log("Request was aborted");
+    }else{
+      console.log("Failed to fetch user data");
+    }
+    
+    
+  }finally{
+    clearTimeout(timeoutId);
+  }
+}
+
+fetchExternalUser();
